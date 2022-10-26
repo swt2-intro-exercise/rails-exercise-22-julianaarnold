af3540ffea1cad2db295e061a0963642ee77f952
Julian <julian@DESKTOP-I3NQ0TP.localdomain>
2022-10-26 17:11:00 +0200

    uncomment test case

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-# describe "New author page", type: :feature do
-#   it "should exist at 'new_author_path' and render withour error" do
-#     # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
-#     visit new_author_path
-#   end
-# end
+describe "New author page", type: :feature do
+  it "should exist at 'new_author_path' and render withour error" do
+    # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
+    visit new_author_path
+  end
+end
