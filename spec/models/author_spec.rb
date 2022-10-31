719ebf2c911be7ecea1ca25f60aaaea71169cdd4
Julian <julian@DESKTOP-I3NQ0TP.localdomain>
2022-10-31 16:28:52 +0100

    author.name test

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -5,4 +5,8 @@ describe "Author model", type: :model do
         expect(author.last_name).to eq('Turing')
         expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
     end
+    it "should have a method 'name' that returns the full name" do
+        author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
+        expect(author.name).to eq('Alan Turing')
+    end
 end
