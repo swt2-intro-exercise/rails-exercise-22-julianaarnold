0989499021e4a7c29510a895bb292d9110fa05f5
Julian <julian@DESKTOP-I3NQ0TP.localdomain>
2022-10-31 16:26:43 +0100

    add author model

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_31_152406) do
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "homepage"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
---
+++ spec/models/author_spec.rb
@@ -0,0 +1,8 @@
+describe "Author model", type: :model do
+    it "should have a first name, last name, and homepage" do
+        author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
+        expect(author.first_name).to eq('Alan')
+        expect(author.last_name).to eq('Turing')
+        expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
+    end
+end
