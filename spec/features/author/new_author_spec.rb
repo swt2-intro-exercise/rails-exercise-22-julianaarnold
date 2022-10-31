652d9cd87a4bf58ee2e01031de3ebc3b8aef0cb0
Julian <julian@DESKTOP-I3NQ0TP.localdomain>
2022-10-31 16:05:07 +0100

    add autor form spec

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,4 +5,10 @@ describe "New author page", type: :feature do
     # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
     visit new_author_path
   end
+  it "should have text input for first name, last name, and homepage" do
+    visit new_author_path
+    expect(page).to have_field('author[first_name]')
+    expect(page).to have_field('author[last_name]')
+    expect(page).to have_field('author[homepage]')
+  end
 end
