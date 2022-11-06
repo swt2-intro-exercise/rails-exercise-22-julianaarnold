50241036c2bde67fbccb4972854cd519acacac66
Julian <julian@DESKTOP-I3NQ0TP.localdomain>
2022-11-07 00:07:58 +0100

    fix #9

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,6 @@
 class Author < ApplicationRecord
+    validates :last_name, presence: true, length: {minimum: 1}
+    
     def name
         first_name + " " + last_name
     end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -9,5 +9,9 @@ describe "Author model", type: :model do
         author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
         expect(author.name).to eq('Alan Turing')
     end
+    it "should not be valid without a last name" do
+        author = Author.new(first_name: 'Alan', homepage: 'http://wikipedia.org/Alan_Turing')
+        expect(author).to_not be_valid
+    end
 
 end
