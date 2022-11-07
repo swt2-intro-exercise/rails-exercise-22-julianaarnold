103e31de08b663ea28a97e6eca8ef8e2b7fecc9d
Julian <julian@DESKTOP-I3NQ0TP.localdomain>
2022-11-07 12:08:52 +0100

    fix #16

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,2 +1,6 @@
 class Paper < ApplicationRecord
+    
+    validates :title, presence: true, length: {minimum: 1}
+    validates :venue, presence: true, length: {minimum: 1}
+    validates :year, presence: true, numericality: { only_integer: true }
 end
--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should not be valid without a title, venue and year" do
+    paper = Paper.new(venue: 'ABQ', year: 2000)
+    expect(paper).to_not be_valid
+    
+    paper = Paper.new(title: 'Science, Bitch!', year: 2000)
+    expect(paper).to_not be_valid
+
+    
+    paper = Paper.new(title: 'Science, Bitch!', venue: 'ABQ')
+    expect(paper).to_not be_valid
+  end
 end
